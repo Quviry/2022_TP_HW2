5971f215851abed822dd3c431da0d5ce0c4e0c75
Quviry <kogekao@ya.ru>
2022-11-04 23:58:31 +0300

    minimal-valid version

--- include/vector.hpp
+++ include/vector.hpp
@@ -13,6 +13,14 @@ requires requires(T x) { x* x; }
 class Vector {
    public:
     Vector();
+    Vector(std::size_t size): size(size), body(new T[size]){}
+    Vector(const Vector<T>& src){
+        T* body = new T[src.get_size()];
+        size = src.get_size();
+        for(unsigned long i = 0; i != size; i++){
+            body[i] = src[i];
+        }
+    }
     Vector(const T* src, std::size_t size) {
         this->size = size;
         this->body = new T[size];
@@ -20,7 +28,6 @@ class Vector {
             this->body[i] = src[i];
         }
     }
-
     template <std::size_t size>
     Vector(const T (&src)[size]) {
         this->size = size;
@@ -31,9 +38,48 @@ class Vector {
     }
     ~Vector() { delete[] this->body; };
 
+    T& operator[](int pos) const { return this->body[pos]; }
+
+    Vector<T>& operator+=(const Vector<T>& other) {
+        for (unsigned long i = 0; i != this->get_size(); i++) {
+            this[i] += other[i];
+        }
+        return this;
+    }
+
+    Vector<T>& operator+(const Vector<T>& other) {
+        Vector<T> result(this);
+        for (unsigned long i = 0; i != result.get_size(); i++) {
+            result[i] += other[i];
+        }
+        return result;
+    }
+
+    Vector<T>& operator-=(const Vector<T>& other) {
+        for (unsigned long i = 0; i != this->get_size(); i++) {
+            this[i] -= other[i];
+        }
+        return this;
+    }
+
+    Vector<T>& operator-(const Vector<T>& other) {
+        Vector<T> result(this);
+        for (unsigned long i = 0; i != result.get_size(); i++) {
+            result[i] -= other[i];
+        }
+        return result;
+    }
+
+    Vector<T>& operator-(const Vector<T>& other) {
+        Vector<T> result(this);
+        for (unsigned long i = 0; i != result.get_size(); i++) {
+            result[i] -= other[i];
+        }
+        return result;
+    }
+
     
-    T operator[](int pos) { return this->body[pos]; }
-    
+
     std::string to_string() {
         std::string result = "";
         for (std::size_t i = 0; i < size; ++i) {
@@ -41,7 +87,7 @@ class Vector {
         }
         return result;
     }
-    const std::size_t get_size() { return size; }
+    std::size_t get_size() const { return size; }
 
    private:
     T* body = nullptr;
